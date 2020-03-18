45b74dd6b997e9a1e2d9ee7dbebd24caa4e1ebf0
Mauricio Saez <saeztoro.mauricio@gmail.com>
2020-03-18 14:36:03 +0100

    testing some stuff

--- jobb_i_procent.py
+++ jobb_i_procent.py
@@ -1,20 +1,14 @@
 
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def main():
+   return render_template('index.html')
 
-    strTable = "<html><table><tr><th>Char</th><th>ASCII</th></tr>"
-
-    for num in range(33,48):
-     symb = chr(num)
-     strRW = "<tr><td>"+str(symb)+ "</td><td>"+str(num)+"</td></tr>"
-     strTable = strTable+strRW
-
-    strTable = strTable+"</table></html>"
-    return(strTable)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
